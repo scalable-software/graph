242fb6fcc57e44904c38596beb005e006d6b6859
Development <development@scalable.software>
2024-06-09 07:45:23 -0700

    OBJECT: ObjectConnection Data Structure -> PASS

---
+++ src/ObjectConnection.meta.ts
@@ -0,0 +1,30 @@
+import { UUID } from "./Utilities/Utilities.js";
+
+/**
+{
+  "id": "d5bc29b2-75ea-4d1a-a0ed-22f4de79a580",
+  "name": "",
+  "source": "35c6779a-fd9d-4089-d1ab-af0b932fc912",
+  "target": "15b6679a-fd9d-4036-b1ab-af0b932fc903",
+  "coordinates": {
+    "start": { "x": 0, "y": 400 },
+    "end": { "x": 100, "y": 400 }
+  }
+}
+*/
+
+export type ObjectCoordinates = {
+  x: number;
+  y: number;
+};
+
+export type ObjectConnectionType = {
+  id: UUID;
+  name: string;
+  source: UUID;
+  target: UUID;
+  coordinates: {
+    start: ObjectCoordinates;
+    end: ObjectCoordinates;
+  };
+};
