85b6927e2c6c9bbb9011c8a5885b63549c6412f2
Development <development@scalable.software>
2024-06-09 07:47:01 -0700

    PACKAGE: Build

---
+++ dist/TupleConnection.d.ts
@@ -0,0 +1,23 @@
+import { StructureType } from "./Node.js";
+import { TupleConnectionType } from "./TupleConnection.meta.js";
+export declare class TupleConnection {
+    static structure: StructureType;
+    static create: ({ name, source, target, coordinates, }: {
+        name: any;
+        source: any;
+        target: any;
+        coordinates: any;
+    }) => TupleConnectionType;
+    static update: (connection: TupleConnectionType, update: TupleConnectionType) => TupleConnectionType;
+    static updateCoordinates: (connection: TupleConnectionType, coordinates: {
+        start: {
+            x: number;
+            y: number;
+        };
+        end: {
+            x: number;
+            y: number;
+        };
+    }) => TupleConnectionType;
+    static translate: (connection: TupleConnectionType, offset: any) => TupleConnectionType;
+}
