3e6abac749086ba39f02f68f11a2618bb84f1bc9
Development <development@scalable.software>
2024-06-09 08:12:07 -0700

    TYPES: Add Connections Types

---
+++ src/Connection.ts
@@ -0,0 +1,5 @@
+import { ObjectConnectionType } from "./ObjectConnection.meta.js";
+import { TupleConnectionType } from "./TupleConnection.meta.js";
+
+export type Connection = ObjectConnectionType | TupleConnectionType;
+export type Connections = Connection[];
