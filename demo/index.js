de1b95d928fc74f1266fec6717f5f230db876fff
Development <development@scalable.software>
2024-06-21 11:19:59 -0700

    DEMO: Update Demo

--- demo/index.js
+++ demo/index.js
@@ -1,3 +1,16 @@
-import { Calculator } from "calculator";
-console.log(`Import module: Calculator`);
-console.log(`Use module: 1 + 2 = ${Calculator.Add(1, 2)}`);
+import { Graph } from "graph";
+console.log(`Import module: Graph`);
+
+console.log(`Define node details`);
+let details = {
+  name: "Node",
+  type: "start",
+  coordinates: { x: 0, y: 0 },
+  icon: "./icon.svg",
+};
+
+console.log(details);
+let node = Graph.createNode(details);
+
+console.log("Create node using static function: Graph.createNode(details)");
+console.log(node);
