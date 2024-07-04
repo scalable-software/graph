fb179ae674e67ba967e8876abb05d045bd9a123a
Development <development@scalable.software>
2024-07-04 05:21:14 -0700

    METHOD: node._get() Private Method Exist -> PASS

--- src/Nodes.ts
+++ src/Nodes.ts
@@ -1,6 +1,9 @@
 export class Nodes extends EventTarget {
   public static init = (nodes = []) => new Nodes(nodes);
 
+  private _get = (target, property, receiver) =>
+    Reflect.get(target, property, receiver);
+
   /**
    * The private constructor is used by the static init method: no direct instantiation is allowed.
    * This is done so that a different return value, other than an instance of the class can be returned.
