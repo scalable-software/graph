3a91baeac6dc08b2f59e5e437ce5c3f15409671f
Development <development@scalable.software>
2024-07-04 04:41:28 -0700

    CLASS: Nodes Exist -> FAIL

---
+++ test/unit/Nodes.test.ts
@@ -0,0 +1,8 @@
+import { Nodes } from "@scalable-software/graph.structure/Nodes";
+
+// Static Members Availability
+describe("Given Nodes imported", () => {
+  it("then Nodes exist", () => {
+    expect(Nodes).toBeDefined();
+  });
+});
