1826bab51a27a55de9d3eb212279146cbdbabc6c
Development <development@scalable.software>
2024-07-04 05:22:18 -0700

    METHOD: node._set() Private Method Exist -> PASS

--- src/Nodes.ts
+++ src/Nodes.ts
@@ -3,7 +3,8 @@ export class Nodes extends EventTarget {
 
   private _get = (target, property, receiver) =>
     Reflect.get(target, property, receiver);
-
+  private _set = (target, property, value, receiver) =>
+    Reflect.set(target, property, value, receiver);
   /**
    * The private constructor is used by the static init method: no direct instantiation is allowed.
    * This is done so that a different return value, other than an instance of the class can be returned.
