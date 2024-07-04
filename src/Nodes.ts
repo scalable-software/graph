00f316dcaf87f47fd057340cd551d9f7606e78ca
Development <development@scalable.software>
2024-07-04 04:52:50 -0700

    DOCUMENTATION: Add Private Constructor Comments

--- src/Nodes.ts
+++ src/Nodes.ts
@@ -1,6 +1,10 @@
 export class Nodes extends EventTarget {
   public static init = (nodes = []) => new Nodes(nodes);
 
+  /**
+   * The private constructor is used by the static init method: no direct instantiation is allowed.
+   * This is done so that a different return value, other than an instance of the class can be returned.
+   */
   constructor(private nodes) {
     super();
   }
