349bccfddb76b254af11a903bf969eb4db7589c6
Development <development@scalable.software>
2024-07-05 07:14:54 -0700

    METHOD: Add property checks in nodes._get()

--- src/Nodes.ts
+++ src/Nodes.ts
@@ -12,8 +12,26 @@ export class Nodes extends EventTarget {
     this._proxy = this._createProxy(nodes);
   }
 
-  private _get = (target, property, receiver) =>
-    Reflect.get(target, property, receiver);
+  private _get = (target, property, receiver) => {
+    const isSymbol = typeof property === "symbol";
+    if (isSymbol) return Reflect.get(target, property, receiver);
+
+    const index = Number(property);
+    const isIndex = Number.isInteger(index);
+    if (isIndex) return Reflect.get(target, property, receiver);
+
+    const isLength = property === "length";
+    if (isLength) return Reflect.get(target, property, receiver);
+
+    const isString = typeof property === "string";
+    if (isString) return Reflect.get(target, property, receiver);
+
+    const isMethod = typeof target[property] === "function";
+    if (isMethod) return Reflect.get(target, property, receiver);
+
+    console.log(property);
+    return Reflect.get(target, property, receiver);
+  };
 
   private _set = (target, property, value, receiver) =>
     Reflect.set(target, property, value, receiver);
