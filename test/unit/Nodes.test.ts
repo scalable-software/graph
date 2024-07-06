a4bb065a407ca89a141924bb4c8db8ccc3af5f17
Development <development@scalable.software>
2024-07-06 11:40:33 -0700

    METHOD: nodes.removeMetadata private method Exists -> FAIL

--- test/unit/Nodes.test.ts
+++ test/unit/Nodes.test.ts
@@ -497,6 +497,9 @@ describe("Given nodes instance", () => {
   it("then nodes.translate exists", () => {
     expect(nodes["translate"]).toBeDefined();
   });
+  it("then nodes.removeMetadata exists", () => {
+    expect(nodes["removeMetadata"]).toBeDefined();
+  });
 });
 
 // Instance Getters Availability
