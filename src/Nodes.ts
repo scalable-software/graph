f3cffd85825b274386b7ed812fbeba93038bd7aa
Development <development@scalable.software>
2024-07-06 06:36:23 -0700

    SETTERS: Switch nodes._set() TriageToSetter

--- src/Nodes.ts
+++ src/Nodes.ts
@@ -2,6 +2,7 @@ export class Nodes extends EventTarget {
   public static init = (nodes = []) => new Nodes(nodes)._proxy;
 
   private _proxy = [];
+  private _result;
 
   /**
    * The private constructor is used by the static init method: no direct instantiation is allowed.
@@ -17,7 +18,7 @@ export class Nodes extends EventTarget {
       Reflect.get(target, property, receiver);
   }
   private set symbol({ target, property, value, receiver }: any) {
-    Reflect.set(target, property, value, receiver);
+    this._result = Reflect.set(target, property, value, receiver);
   }
 
   private get index() {
@@ -25,12 +26,15 @@ export class Nodes extends EventTarget {
       Reflect.get(target, property, receiver);
   }
   private set index({ target, property, value, receiver }: any) {
-    Reflect.set(target, property, value, receiver);
+    this._result = Reflect.set(target, property, value, receiver);
   }
 
   private get length() {
     return (target, property, receiver) =>
       Reflect.get(target, property, receiver);
+  }
+  private set length({ target, property, value, receiver }: any) {
+    this._result = Reflect.set(target, property, value, receiver);
   }
 
   private get property() {
@@ -38,27 +42,35 @@ export class Nodes extends EventTarget {
       Reflect.get(target, property, receiver);
   }
   private set property({ target, property, value, receiver }: any) {
-    Reflect.set(target, property, value, receiver);
+    this._result = Reflect.set(target, property, value, receiver);
   }
 
   private get method() {
     return (target, property, receiver) =>
       Reflect.get(target, property, receiver);
   }
+  private set method({ target, property, value, receiver }: any) {
+    this._result = Reflect.set(target, property, value, receiver);
+  }
 
   private get default() {
     return (target, property, receiver) =>
       Reflect.get(target, property, receiver);
   }
+  private set default({ target, property, value, receiver }: any) {
+    this._result = Reflect.set(target, property, value, receiver);
+  }
 
   private _get = (target, property, receiver) =>
     this[this._getPropertyType(property, target)](target, property, receiver);
 
-  private _set = (target, property, value, receiver) => {
-    const type = this._getPropertyType(property, target);
-    console.log(`${type}`);
-    return Reflect.set(target, property, value, receiver);
-  };
+  private _set = (target, property, value, receiver) =>
+    (this[this._getPropertyType(property, target)] = {
+      target,
+      property,
+      value,
+      receiver,
+    }) && this._result;
 
   private _createProxy = (target) =>
     new Proxy(target, { get: this._get, set: this._set });
