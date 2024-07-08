79be28e292c0c10ab968e92809d8335b7995ac47
Development <development@scalable.software>
2024-07-08 08:09:14 -0700

    COMPONENT: Connections Exists -> FAIL

---
+++ test/unit/Connections.test.ts
@@ -0,0 +1,7 @@
+import { Connections } from "@scalable-software/graph.structure/Connections";
+
+describe("Given Connections imported", () => {
+  it("then Connections exist", () => {
+    expect(Connections).toBeDefined();
+  });
+});
