7e2cab608a33ddd18402df4e62ca54570c476ae9
Development <development@scalable.software>
2024-07-12 10:08:06 -0700

    CONSTRUCTOR:  new Nodes() ReturnsInstanceOfArray -> FAIL

--- test/unit/Nodes.Extended.test.ts
+++ test/unit/Nodes.Extended.test.ts
@@ -1,8 +1,16 @@
 import { Nodes } from "../../src/Nodes.Extended.js";
 
-// Instance Properties Availability
+// Class Availability
 describe("Given Nodes imported", () => {
   it("then Nodes exist", () => {
     expect(Nodes).toBeDefined();
+  });
+});
+
+// Constructor Behavior
+describe("Given Nodes instantiated", () => {
+  it("then Nodes is an instance of Array", () => {
+    const nodes = new Nodes();
+    expect(nodes).toBeInstanceOf(Array);
   });
 });
