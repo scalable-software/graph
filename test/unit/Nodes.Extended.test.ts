42258110314222dba79e733ea4ab2c6abc63644e
Development <development@scalable.software>
2024-07-12 10:03:29 -0700

    CLASS: Nodes Exists -> FAIL

---
+++ test/unit/Nodes.Extended.test.ts
@@ -0,0 +1,8 @@
+import { Nodes } from "../../src/Nodes.Extended.js";
+
+// Instance Properties Availability
+describe("Given Nodes imported", () => {
+  it("then Nodes exist", () => {
+    expect(Nodes).toBeDefined();
+  });
+});
